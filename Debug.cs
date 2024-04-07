f5a6ae72ea2b0d3c749b1791aac91663bb2642c6
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-04-07 03:00:46 +0100

    - sneaking improvements (don't fall off the block, camera)

--- Debug.cs
+++ Debug.cs
@@ -21,11 +21,48 @@ public class Debug {
         debugShader.View = GameScreen.world.player.camera.getViewMatrix(interp);
     }
 
-    public void drawLine(Vector3D<double> from, Vector3D<double> to, Color4b colour) {
+    public void drawLine(Vector3D<double> from, Vector3D<double> to, Color4b colour = default) {
+        if (colour == default) {
+            colour = Color4b.Red;
+        }
+        if (currentLine >= MAX_LINE_VERTICES - 2) {
+            flushLines();
+        }
+        //Console.Out.WriteLine(currentLine);
         lineVertices[currentLine] = new VertexColor(from.toVec3(), colour);
         currentLine++;
         lineVertices[currentLine] = new VertexColor(to.toVec3(), colour);
         currentLine++;
+    }
+
+    public void drawAABB(AABB aabb, Color4b colour = default) {
+        // corners
+        var lsw = aabb.min;
+        var lse = new Vector3D<double>(aabb.maxX, aabb.minY, aabb.minZ);
+        var lnw = new Vector3D<double>(aabb.minX, aabb.minY, aabb.maxZ);
+        var lne = new Vector3D<double>(aabb.maxX, aabb.minY, aabb.maxZ);
+
+        var usw = new Vector3D<double>(aabb.minX, aabb.maxY, aabb.minZ);
+        var use = new Vector3D<double>(aabb.maxX, aabb.maxY, aabb.minZ);
+        var unw = new Vector3D<double>(aabb.minX, aabb.maxY, aabb.maxZ);
+        var une = new Vector3D<double>(aabb.maxX, aabb.maxY, aabb.maxZ);
+
+        // join them with lines
+        drawLine(lsw, lse);
+        drawLine(lse, lne);
+        drawLine(lne, lnw);
+        drawLine(lnw, lsw);
+
+        drawLine(usw, use);
+        drawLine(use, une);
+        drawLine(une, unw);
+        drawLine(unw, usw);
+
+        // draw columns
+        drawLine(lsw, usw);
+        drawLine(lse, use);
+        drawLine(lne, une);
+        drawLine(lnw, unw);
     }
 
     public void flushLines() {
