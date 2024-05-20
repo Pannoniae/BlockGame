9202dedff470fd5d50130e7023ea25004a4936fa
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-05-20 03:43:40 +0100

    - fix action timing

--- world/TickAction.cs
+++ world/TickAction.cs
@@ -1,8 +1,3 @@
 namespace BlockGame;
 
-public record TickAction(Action action, int tick) {
-    /// <summary>
-    /// Should this action happen or is it disabled? (disabled timers are scheduled for deletion in the queue)
-    /// </summary>
-    public bool enabled = true;
-}
+public record TickAction(Action action, int tick);
