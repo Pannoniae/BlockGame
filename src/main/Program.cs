6ad1ffdc74b2d3f21175c3fa1bdb8087c6082a87
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-07-01 22:26:37 +0100

    test

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -16,15 +16,6 @@ public class Program {
     }
     public static void handleCrash(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs) {
         var e = (Exception)unhandledExceptionEventArgs.ExceptionObject;
-        // call CrashReporter
-        using var crashReporterProc = new Process {
-            StartInfo = new ProcessStartInfo {
-                FileName = "CrashReporter",
-                RedirectStandardOutput = true,
-                UseShellExecute = false
-            }
-        };
-        crashReporterProc.Start();
 
         Console.Out.WriteLine("Your game crashed! Here are some relevant details:");
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
