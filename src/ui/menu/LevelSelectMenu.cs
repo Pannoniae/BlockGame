693613e3210410aee52710019de6f678074da122
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-07-17 00:50:47 +0100

    .

--- src/ui/menu/LevelSelectMenu.cs
+++ src/ui/menu/LevelSelectMenu.cs
@@ -10,41 +10,6 @@ public class LevelSelectMenu : Menu {
     public bool[] worldExists = new bool[NUM_LEVELS];
 
     public LevelSelectMenu() {
-        /*var newLevelButton = new Button(this, "newLevel", true, "New Level");
-        newLevelButton.setPosition(new Vector2D<int>(0, -64));
-        newLevelButton.centreContents();
-        newLevelButton.clicked += () => {
-            if (!Directory.Exists("level")) {
-                Directory.CreateDirectory("level");
-            }
-            // if existing world, delete files from level directory
-            else {
-                foreach (var file in Directory.GetFiles("level")) {
-                    File.Delete(file);
-                }
-            }
-            var seed = Random.Shared.Next(int.MaxValue);
-            var world =  new World("level1", seed);
-            Screen.GAME_SCREEN.setWorld(world);
-            world.loadAroundPlayer();
-            Game.instance.switchToScreen(Screen.GAME_SCREEN);
-            Game.instance.lockMouse();
-        };
-        var loadLevelButton = new Button(this, "loadLevel", true, "Load Level");
-        loadLevelButton.setPosition(new Vector2D<int>(0, -32));
-        loadLevelButton.centreContents();
-        loadLevelButton.clicked += () => {
-            if (!WorldIO.worldExists("level1")) {
-                return;
-            }
-            var world = WorldIO.load("level");
-            Screen.GAME_SCREEN.setWorld(world);
-            world.loadAroundPlayer();
-            Game.instance.switchToScreen(Screen.GAME_SCREEN);
-            Game.instance.lockMouse();
-        };*/
-
-
         // create level buttons
         for (int i = 0; i < NUM_LEVELS; i++) {
             var levelIndex = i + 1;
@@ -54,7 +19,6 @@ public class LevelSelectMenu : Menu {
             levelButton.clicked += loadLevel;
             addElement(levelButton);
         }
-
 
         var backButton = new Button(this, "back", false, "Back") {
             horizontalAnchor = HorizontalAnchor.LEFT,
