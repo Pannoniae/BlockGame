105919de4b05b4d63fdb36a822df227cf2271a22
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2025-08-31 12:56:44 +0100

    flowing water textures

--- src/world/block/Water.cs
+++ src/world/block/Water.cs
@@ -66,11 +66,10 @@ public class Water : Block {
         currentLevel = setFalling((byte)currentLevel, isFalling(data));
         var falling = isFalling(data);
 
-        
-        
+
         bool newFalling = false;
         int newLevel;
-        
+
         // is this a source? (any kind)
         // if falling, update it anyway because we might have to remove
         // is this a source? great, sleep well
@@ -99,12 +98,12 @@ public class Water : Block {
                 // do we still need this? IDK
                 newLevel = -1;
             }
-            
+
             // EXCEPTION: if one of the neighbours is falling water with data 7, give it one more chance so it doesn't look shite
             if (hasFalling && bestLevel == maxFlow - 1) {
                 bestLevel = maxFlow - 2;
             }
-            
+
             newLevel = bestLevel + 1;
             if (newLevel >= maxFlow) {
                 newLevel = -1; // too weak to exist
@@ -171,7 +170,7 @@ public class Water : Block {
                 }
             }
         }
-        
+
         // Step 10-12: Spreading logic
         // where's the other steps? I rewrote the whole thing 4 times already, they've disappeared, sorry
         if (currentLevel == -1) {
@@ -200,17 +199,16 @@ public class Water : Block {
             // if it's NOT solid below, don't even bother (otherwise the fucking waterfalls will go off to the side endlessly because there's water below)
             var isSolid = fullBlock[world.getBlock(x, y - 1, z)];
             if (currentLevel == 0 || (isSolid && !canSpread(world, x, y - 1, z))) {
-                
                 // if it's falling, it should be at least level 1 when spreading sideways
                 // todo is this really necessary?
 
                 var isFalling = Water.isFalling((byte)currentLevel);
-                
+
                 // alternatively, chop it down!
                 currentLevel = setFalling((byte)currentLevel, false);
-                
+
                 newLevel = currentLevel + 1;
-                
+
                 // EXCEPTION: if falling water with data 7, give it one more chance so it doesn't look shite
                 if (isFalling && currentLevel == maxFlow - 1) {
                     newLevel = maxFlow - 1;
@@ -359,7 +357,6 @@ public class Water : Block {
     }
 
     public override void interact(World world, int x, int y, int z, Entity e) {
-        
         e.inLiquid = true;
         // push entity in flow direction
         var flow = getFlow(world, x, y, z);
@@ -378,27 +375,27 @@ public class Water : Block {
         if (block != Blocks.WATER) {
             return Vector3.Zero;
         }
-    
+
         var metadata = world.getBlockMetadata(x, y, z);
         var currentHeight = getHeight(metadata);
         var flow = Vector3.Zero;
-    
+
         // Check all 6 directions for height differences
         // Down
         var blockBelow = world.getBlock(x, y - 1, z);
         if (blockBelow == Blocks.AIR || (blockBelow == Blocks.WATER && getRenderWater(world, x, y - 1, z) > 0)) {
             flow.Y -= 1.0f; // Strong downward flow
         }
-    
+
         // Horizontal directions
         Vector3I[] horizontalDirs = [
             new Vector3I(-1, 0, 0), new Vector3I(1, 0, 0),
             new Vector3I(0, 0, -1), new Vector3I(0, 0, 1)
         ];
-    
+
         foreach (var dir in horizontalDirs) {
             var neighborBlock = world.getBlock(x + dir.X, y + dir.Y, z + dir.Z);
-        
+
             if (neighborBlock == Blocks.AIR) {
                 // Flow toward empty space
                 flow += new Vector3(dir.X * 0.5f, dir.Y * 0.5f, dir.Z * 0.5f);
@@ -415,12 +412,12 @@ public class Water : Block {
                 }
             }
         }
-    
+
         // Check if water above is pushing down
         if (world.getBlock(x, y + 1, z) == Blocks.WATER) {
             flow.Y -= 0.5f;
         }
-    
+
         return flow;
     }
 
@@ -471,7 +468,7 @@ public class Water : Block {
         if (same) {
             return false;
         }
-        
+
         var notTransparent = !transparent[br.getBlockCached(direction.X, direction.Y, direction.Z)];
 
         return dir == RawDirection.UP || (notTransparent && base.cullFace(br, x, y, z, dir));
@@ -487,18 +484,29 @@ public class Water : Block {
 
         // Calculate corner heights for sloped rendering
         var world = br.world;
-        var blockPos = new Vector3I(x, y, z);
         var h00 = getRenderHeight(world, x, y, z, -1, -1); // southwest
-        var h10 = getRenderHeight(world, x, y, z, 1, -1); // southeast  
+        var h10 = getRenderHeight(world, x, y, z, 1, -1); // southeast
         var h01 = getRenderHeight(world, x, y, z, -1, 1); // northwest
         var h11 = getRenderHeight(world, x, y, z, 1, 1); // northeast
 
+        // Calculate flow direction for UV rotation
+        var flow = getFlow(world, x, y, z);
+        float flowAngle = 0;
+        if (flow.X != 0 || flow.Z != 0) {
+            flowAngle = MathF.Atan2(flow.Z, flow.X);
+        }
+
+        float flowCos = MathF.Cos(flowAngle);
+        // why negative? because UV Y axis is inverted
+        float flowSin = -MathF.Sin(flowAngle);
+
+        // Use center 16x16 of the 32x32 flowing texture
+        var flowingUV = uvs[1] + 0.25f; // offset to center 16x16
+        const float flowingUVSize = 1f;
+
         // Get texture coordinates for water
-        var texMin =
-            (level > 0 || falling)
-                ? uvs[1]
-                : uvs[0]; // flowing texture for levels > 0 or falling water, still texture for source
-        var texMax = texMin + 1;
+        var texMin = (level > 0 || falling) ? flowingUV : uvs[0];
+        var texMax = (level > 0 || falling) ? flowingUV + flowingUVSize : uvs[0] + 1;
         var min = texCoords(texMin.u, texMin.v);
         var max = texCoords(texMax.u, texMax.v);
 
@@ -506,9 +514,9 @@ public class Water : Block {
         var vMin = min.Y;
         var uMax = max.X;
         var vMax = max.Y;
-
-        // max possible height for UV calculation
-        const float maxHeight = 1.0f;
+        
+        // Cache v range for interpolation
+        var vRange = vMax - vMin;
 
         Span<BlockVertexPacked> cache = stackalloc BlockVertexPacked[4];
         Span<Vector4> colourCache = stackalloc Vector4[4];
@@ -517,6 +525,29 @@ public class Water : Block {
         x &= 15;
         y &= 15;
         z &= 15;
+
+        // Calculate UVs relative to center for rotation
+        float uCenter = (uMin + uMax) * 0.5f;
+        float vCenter = (vMin + vMax) * 0.5f;
+
+        // --- Top Face UV Rotation ---
+        float u0 = uMin - uCenter;
+        float u1 = uMax - uCenter;
+        float v0 = vMin - vCenter;
+        float v1 = vMax - vCenter;
+        
+        var uv00 = rotateUV(u0, v1, flowCos, flowSin) + new Vector2(uCenter, vCenter);
+        var uv01 = rotateUV(u0, v0, flowCos, flowSin) + new Vector2(uCenter, vCenter);
+        var uv10 = rotateUV(u1, v0, flowCos, flowSin) + new Vector2(uCenter, vCenter);
+        var uv11 = rotateUV(u1, v1, flowCos, flowSin) + new Vector2(uCenter, vCenter);
+
+        // --- Side Face UV Rotation (-90 degrees) ---
+        // Transformation: T(u,v) = (v - vCenter + uCenter, -u + uCenter + vCenter)
+        float uNewSideOffset = uCenter - vCenter;
+        float vNewSideOffset = uCenter + vCenter;
+        
+        float vNewSideUMin = -uMin + vNewSideOffset;
+        float vNewSideUMax = -uMax + vNewSideOffset;
 
         for (RawDirection d = 0; d < RawDirection.MAX; d++) {
             if (cullFace(br, x, y, z, d)) {
@@ -533,28 +564,28 @@ public class Water : Block {
 
                 switch (d) {
                     case RawDirection.WEST:
-                        br.vertex(x + 0, y + h01, z + 1, uMin, vMax - (vMax - vMin) * (h01));
-                        br.vertex(x + 0, y + 0, z + 1, uMin, vMax);
-                        br.vertex(x + 0, y + 0, z + 0, uMax, vMax);
-                        br.vertex(x + 0, y + h00, z + 0, uMax, vMax - (vMax - vMin) * (h00));
+                        br.vertex(x + 0, y + h01, z + 1, (vMax - vRange * h01) + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 0, y + 0, z + 1, vMax + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 0, y + 0, z + 0, vMax + uNewSideOffset, vNewSideUMax);
+                        br.vertex(x + 0, y + h00, z + 0, (vMax - vRange * h00) + uNewSideOffset, vNewSideUMax);
                         break;
                     case RawDirection.EAST:
-                        br.vertex(x + 1, y + h10, z + 0, uMin, vMax - (vMax - vMin) * (h10));
-                        br.vertex(x + 1, y + 0, z + 0, uMin, vMax);
-                        br.vertex(x + 1, y + 0, z + 1, uMax, vMax);
-                        br.vertex(x + 1, y + h11, z + 1, uMax, vMax - (vMax - vMin) * (h11));
+                        br.vertex(x + 1, y + h10, z + 0, (vMax - vRange * h10) + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 1, y + 0, z + 0, vMax + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 1, y + 0, z + 1, vMax + uNewSideOffset, vNewSideUMax);
+                        br.vertex(x + 1, y + h11, z + 1, (vMax - vRange * h11) + uNewSideOffset, vNewSideUMax);
                         break;
                     case RawDirection.SOUTH:
-                        br.vertex(x + 0, y + h00, z + 0, uMin, vMax - (vMax - vMin) * (h00));
-                        br.vertex(x + 0, y + 0, z + 0, uMin, vMax);
-                        br.vertex(x + 1, y + 0, z + 0, uMax, vMax);
-                        br.vertex(x + 1, y + h10, z + 0, uMax, vMax - (vMax - vMin) * (h10));
+                        br.vertex(x + 0, y + h00, z + 0, (vMax - vRange * h00) + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 0, y + 0, z + 0, vMax + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 1, y + 0, z + 0, vMax + uNewSideOffset, vNewSideUMax);
+                        br.vertex(x + 1, y + h10, z + 0, (vMax - vRange * h10) + uNewSideOffset, vNewSideUMax);
                         break;
                     case RawDirection.NORTH:
-                        br.vertex(x + 1, y + h11, z + 1, uMin, vMax - (vMax - vMin) * (h11));
-                        br.vertex(x + 1, y + 0, z + 1, uMin, vMax);
-                        br.vertex(x + 0, y + 0, z + 1, uMax, vMax);
-                        br.vertex(x + 0, y + h01, z + 1, uMax, vMax - (vMax - vMin) * (h01));
+                        br.vertex(x + 1, y + h11, z + 1, (vMax - vRange * h11) + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 1, y + 0, z + 1, vMax + uNewSideOffset, vNewSideUMin);
+                        br.vertex(x + 0, y + 0, z + 1, vMax + uNewSideOffset, vNewSideUMax);
+                        br.vertex(x + 0, y + h01, z + 1, (vMax - vRange * h01) + uNewSideOffset, vNewSideUMax);
                         break;
                     case RawDirection.DOWN:
                         br.vertex(x + 1, y + 0, z + 1, uMin, vMin);
@@ -563,16 +594,20 @@ public class Water : Block {
                         br.vertex(x + 0, y + 0, z + 1, uMax, vMin);
                         break;
                     case RawDirection.UP:
-                        // sloped top face with corner heights
-                        br.vertex(x + 0, y + h01, z + 1, uMin, vMin);
-                        br.vertex(x + 0, y + h00, z + 0, uMin, vMax);
-                        br.vertex(x + 1, y + h10, z + 0, uMax, vMax);
-                        br.vertex(x + 1, y + h11, z + 1, uMax, vMin);
+                        br.vertex(x + 0, y + h01, z + 1, uv00.X, uv00.Y);
+                        br.vertex(x + 0, y + h00, z + 0, uv01.X, uv01.Y);
+                        br.vertex(x + 1, y + h10, z + 0, uv10.X, uv10.Y);
+                        br.vertex(x + 1, y + h11, z + 1, uv11.X, uv11.Y);
+
                         break;
                 }
 
                 br.end(vertices);
             }
         }
+    }
+
+    private static Vector2 rotateUV(float u, float v, float cos, float sin) {
+        return new Vector2(u * cos - v * sin, u * sin + v * cos);
     }
 }
