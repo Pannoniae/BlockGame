e02af830e51cc31f482f55511c1aa67d4682914e
kismama <159849319+kismama@users.noreply.github.com>
2025-11-04 00:40:16 +0000

    pigmodel updated

---
+++ src/render/model/PigModel.cs
@@ -0,0 +1,61 @@
+using System.Numerics;
+using BlockGame.main;
+using BlockGame.util;
+using BlockGame.world;
+
+namespace BlockGame.render.model;
+
+public class PigModel : EntityModel {
+
+    public const int xs = 64;
+    public const int ys = 36;
+    public readonly Cube head;
+    public readonly Cube body;
+    public readonly Cube frontRightLeg;
+    public readonly Cube frontLeftLeg;
+    public readonly Cube backLeftLeg;
+    public readonly Cube backRightLeg;
+
+    public PigModel(int l) {
+
+        head = new Cube().pos(0, l + 4, 12).off(-3, 0, -4).ext(6, 6, 6).tex(0, 0).gen(xs, ys);
+        body = new Cube().pos(0, l + 8, 0).off(-5, -8, -8).ext(10, 8, 16).tex(12, 12).gen(xs, ys);
+
+        frontRightLeg = new Cube().pos(-2.5f, l, 6f).off(-1.5f, -l, -1.5f).ext(3, l, 3).tex(0, 12).gen(xs, ys);
+        frontLeftLeg = new Cube().pos(2.5f, l, 6f).off(-1.5f, -l, -1.5f).ext(3, l, 3).tex(0, 12).gen(xs, ys);
+        backLeftLeg = new Cube().pos(2.5f, l, -4.5f).off(-1.5f, -l, -1.5f).ext(3, l, 3).tex(0, 12).gen(xs, ys);
+        backRightLeg = new Cube().pos(-2.5f, l, -4.5f).off(-1.5f, -l, -1.5f).ext(3, l, 3).tex(0, 12).gen(xs, ys);
+    }
+
+
+    public override void render(MatrixStack mat, Entity e, float apos, float aspeed, float scale, double interp) {
+        // set cow texture
+        Game.graphics.tex(0, e.tex);
+
+        // calculate interpolated rotations
+        var interpRot = Vector3.Lerp(e.prevRotation, e.rotation, (float)interp);
+        var interpBodyRot = Vector3.Lerp(e.prevBodyRotation, e.bodyRotation, (float)interp);
+
+        // calculate head rotation relative to body
+        var headRotY = interpRot.Y - interpBodyRot.Y;
+
+        // render body (no animation)
+        body.render(mat, scale);
+
+        head.rotation = new Vector3(0, headRotY, 0);
+        head.render(mat, scale);
+
+        float cs = Meth.clamp(aspeed, 0, 1);
+        float lr = MathF.Sin(apos * 10) * 25f * cs * Meth.phiF;
+
+        frontRightLeg.rotation = new Vector3(lr, 0, 0);
+        frontLeftLeg.rotation = new Vector3(-lr, 0, 0);
+        backRightLeg.rotation = new Vector3(-lr, 0, 0);
+        backLeftLeg.rotation = new Vector3(lr, 0, 0);
+
+        frontRightLeg.render(mat, scale);
+        frontLeftLeg.render(mat, scale);
+        backRightLeg.render(mat, scale);
+        backLeftLeg.render(mat, scale);
+    }
+}
